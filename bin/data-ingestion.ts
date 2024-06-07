a2f872ff88c1f61ed844b7255c74964023d174f6
Matthieu <matthieu.lienart@gmail.com>
2024-06-07 18:44:46 +0200

    feat:use own fork and codestar connection

--- bin/data-ingestion.ts
+++ bin/data-ingestion.ts
@@ -32,8 +32,8 @@ new DataIngestionPipelineStack(app, `${prefix}-DataIngestionPipelineStack`, {
     region: process.env.CDK_DEFAULT_REGION
   },
   prefix: prefix,
-  repoName: 'amanoxsolutions/mlops-realtime-data-ingestion',
-  codestarConnectionName: 'mlops-realtime-data-ingestion',
+  repoName: 'mlnrt/mlops-realtime-data-ingestion',
+  codestarConnectionName: 'personal-codestar-github',
   fullBranchName: fullBranchName,
   shortBranchName: shortBranchName,
 });
